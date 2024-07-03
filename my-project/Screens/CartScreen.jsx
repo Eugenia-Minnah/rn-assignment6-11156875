8d1d0d2c4efbfcbc1348d62a55c4ef866759269d
Eugenia Minnah <eaminnah@st.ug.edu.gh>
2024-07-03 17:28:19 +0000

    Styled my Checkout text in my Cart screen

--- my-project/Screens/CartScreen.jsx
+++ my-project/Screens/CartScreen.jsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 export default function CartScreen() {
   return (
-    <View>
-      <Text>CartScreen</Text>
+    <View style={styles.container}>
+      <View style={{ flexDirection: 'row', marginVertical: 20 }}>
+        <Image source={require('../assets/Logo.png')} style={{ marginRight: 100, marginLeft: 150 }} />
+        <Image source={require('../assets/Search.png')} style={{ left: 30 }} />
+      </View>
+      <Text style={{ marginVertical: 10, textAlign: 'center', fontSize: 22, fontFamily:'Times New Roman', textDecorationLine: 'underline'  }}>CHECKOUT</Text>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
